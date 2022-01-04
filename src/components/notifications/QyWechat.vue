1216d3ee3d4b0e0f02984a7f1f6d090cce6ca1d1
chillley <chillley@163.com>
2022-01-04 10:40:30 +0800

    add chinese translation

--- src/components/notifications/QyWechat.vue
+++ src/components/notifications/QyWechat.vue
@@ -2,5 +2,14 @@
     <div class="mb-3">
         <label for="WebHookUrl" class="form-label">{{ $t("WebHookUrl") }}<span style="color: red;"><sup>*</sup></span></label>
         <input id="WebHookUrl" v-model="$parent.notification.qyWeChatWebHookUrl" type="text" class="form-control" required>
+        <div class="form-text">
+            <p><span style="color: red;"><sup>*</sup></span>{{ $t("Required") }}</p>
+        </div>
+        <i18n-t tag="div" keypath="wayToGetTeamsURL" class="form-text">
+            <a
+                href="https://work.weixin.qq.com/api/doc/90000/90136/91770"
+                target="_blank"
+            >{{ $t("here") }}</a>
+        </i18n-t>
     </div>
 </template>
